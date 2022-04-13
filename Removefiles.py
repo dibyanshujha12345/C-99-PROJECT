0d9a96832c3bb18d88ba22149dbdb5f1ec000543
DIBYANSHU JHA <85605269+dibyanshujha12345@users.noreply.github.com>
2022-04-13 16:24:32 +0530

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,17 @@
+import os,shutil,time
+
+path=input("Enter the path of the file")
+path=path+/
+
+
+days=30
+days=days.time.time()
+
+if os.path.exists(path)==true:
+    os.walk(path)
+    os.path.join()    
+
+
+
+
+
